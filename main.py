32080c4d679c773974a871b87f30dadfcab7a208
doesdoing <doesdoing@hotmail.com>
2019-04-26 10:09:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import json
 import sqlite3_plus
 import _find
 from random_plus import random_plus
+from flask_compress import Compress
 app = Flask(__name__, static_url_path='')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=6)
 app.jinja_env.variable_start_string = '%%'
 app.jinja_env.variable_end_string = '%%'
+Compress(app)
 
 
 @app.route("/api/<what>", methods=['GET'])
@@ -22,24 +24,27 @@ def api_message(what):
         data_info['data'] = []
         data = []
         cookies = request.cookies.get('key')
-        data_val = sqlite3_plus.sqlite3_plus(Path='./sql/user.db', Tab='user').find(cookies=cookies)
+        data_val = sqlite3_plus.sqlite3_plus(
+            Path='./sql/user.db', Tab='user').find(cookies=cookies)
         data_info['login_user'] = data_val[0]['name']
         data_info['level'] = data_val[0]['level']
         data_info['personal_img'] = data_val[0]['personal_img'] if data_val[0]['personal_img'] else './image/bill.jpg'
         start = request.args.get('start') if request.args.get('start')else ''
         end = request.args.get('end') if request.args.get('end')else ''
-        arr_what=['user','server','network','sql','system','upload']
+        arr_what = ['user', 'server', 'network', 'sql', 'system', 'upload']
         if what:
             if what in arr_what:
-                if what=='user':
+                if what == 'user':
                     if data_info['level'] == 'admin':
-                        data = sqlite3_plus.sqlite3_plus(Path='./sql/'+what+'.db', Tab=what).find()
+                        data = sqlite3_plus.sqlite3_plus(
+                            Path='./sql/'+what+'.db', Tab=what).find()
                         data_info['len'] = len(data)
                     else:
                         data = data_val
                         data_info['len'] = len(data_val)
                 else:
-                    data = sqlite3_plus.sqlite3_plus(Path='./sql/'+what+'.db', Tab=what).find()
+                    data = sqlite3_plus.sqlite3_plus(
+                        Path='./sql/'+what+'.db', Tab=what).find()
                     data_info['len'] = len(data)
         if start and end:
             _IP = request.args.get('ip')
@@ -49,13 +54,15 @@ def api_message(what):
             _SYS_FILE = request.args.get('SFN')
             _USERNAME = request.args.get('username')
             _LEVEL = request.args.get('level')
-            _ARR={'ip':_IP,'location':_LOCATION,'projects':_PROJECTS,'name':_NAME,'src_name':_SYS_FILE,'username':_USERNAME,'level':_LEVEL}
-            e1=''
+            _ARR = {'ip': _IP, 'location': _LOCATION, 'projects': _PROJECTS,
+                    'name': _NAME, 'src_name': _SYS_FILE, 'username': _USERNAME, 'level': _LEVEL}
+            e1 = ''
             if _IP or _LOCATION or _PROJECTS or _NAME or _SYS_FILE or _USERNAME or _LEVEL:
                 print(_ARR)
                 for x in _ARR:
                     if _ARR[x]:
-                        e1 = _find.find(target=x ,start=start,end=end, data=data, value=_ARR[x])
+                        e1 = _find.find(target=x, start=start,
+                                        end=end, data=data, value=_ARR[x])
                 data_info['data'] = e1['data']
                 data_info['len'] = e1['len']
             else:
@@ -68,7 +75,8 @@ def index(index):
     req = json.dumps(request.args)
     if req == "{}":
         cookies = request.cookies.get('key')
-        data_val = sqlite3_plus.sqlite3_plus(Path='./sql/user.db', Tab='user').find(cookies=cookies)
+        data_val = sqlite3_plus.sqlite3_plus(
+            Path='./sql/user.db', Tab='user').find(cookies=cookies)
         if index == 'login':
             return app.send_static_file('html/login.html')
         if cookies:
@@ -76,13 +84,13 @@ def index(index):
                 if index:
                     return app.send_static_file('html/'+index+'.html')
                 else:
-                    return redirect(url_for('index'))
+                    return redirect('index')
             else:
-                return redirect(url_for('login'))
+                return redirect('login')
         else:
-            return redirect(url_for('login'))
+            return redirect('login')
     else:
-        return redirect(url_for('login'))
+        return redirect('login')
 
 
 @app.route('/')
@@ -92,11 +100,9 @@ def user():
         cookies = request.cookies.get('key')
         data_val = sqlite3_plus.sqlite3_plus(
             Path='./sql/user.db', Tab='user').find(cookies=cookies)
-        if cookies == None or cookies == '':
-            return redirect('/login')
         if data_val:
             if cookies == data_val[0]['cookies']:
-                return redirect(url_for('index'))
+                return app.send_static_file('html/index.html')
         else:
             return redirect('/login')
     else:
@@ -110,16 +116,18 @@ def set_cookie():
     Pass = request.form['password']
     if request.method == 'POST':
         if name != '' and Pass != '':
-            value = sqlite3_plus.sqlite3_plus(Path='./sql/user.db', Tab='user').find(username=name)
+            value = sqlite3_plus.sqlite3_plus(
+                Path='./sql/user.db', Tab='user').find(username=name)
             if value != []:
-                    if (name in value[0]['username'] and Pass in value[0]['password']):
-                        create_cookies = random_plus().random_plus(Many=50)
-                        sqlite3_plus.sqlite3_plus(Path='./sql/user.db', Tab='user').update(Data={'cookies': create_cookies, 'ip': ip}, username=name)
-                        res = make_response(redirect('/index'))
-                        res.set_cookie("key", create_cookies, max_age=60*60)
-                        return res
-                    else:
-                        return redirect('/')
+                if (name == value[0]['username'] and Pass == value[0]['password']):
+                    create_cookies = random_plus().random_plus(Many=50)
+                    sqlite3_plus.sqlite3_plus(Path='./sql/user.db', Tab='user').update(
+                        Data={'cookies': create_cookies, 'ip': ip}, username=name)
+                    res = make_response(redirect('/index'))
+                    res.set_cookie("key", create_cookies, max_age=60*60)
+                    return res
+                else:
+                    return redirect('/')
             else:
                 return redirect('/')
     else:
@@ -147,11 +155,14 @@ def delete_cookie():
 def fuc(do, what):
     post_data = json.loads(request.get_data())
     if do == 'add':
-        sqlite3_plus.sqlite3_plus(Path='./sql/'+what+'.db', Tab=what).add(Data=post_data)
+        sqlite3_plus.sqlite3_plus(
+            Path='./sql/'+what+'.db', Tab=what).add(Data=post_data)
     elif do == 'del':
-        sqlite3_plus.sqlite3_plus(Path='./sql/'+what+'.db', Tab=what).delete(ID=post_data['ID'])
+        sqlite3_plus.sqlite3_plus(
+            Path='./sql/'+what+'.db', Tab=what).delete(ID=post_data['ID'])
     elif do == 'change':
-        sqlite3_plus.sqlite3_plus(Path='./sql/'+what+'.db', Tab=what).update(Data=post_data, ID=post_data['ID'])
+        sqlite3_plus.sqlite3_plus(
+            Path='./sql/'+what+'.db', Tab=what).update(Data=post_data, ID=post_data['ID'])
     return 'done'
 
 
@@ -163,31 +174,39 @@ def Download(filename):
         for x in lists:
             if filename in x:
                 if os.path.isfile(os.path.join(Path, x)):
-                    data = sqlite3_plus.sqlite3_plus(Path='./sql/upload.db', Tab='upload').find(sys_name=filename)
+                    data = sqlite3_plus.sqlite3_plus(
+                        Path='./sql/upload.db', Tab='upload').find(sys_name=filename)
                     return send_from_directory(Path, x, as_attachment=True, attachment_filename=data[0]['src_name']+'.'+data[0]['file_type'])
 
 
 @app.route('/upload/file', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        file_class = ['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG','xlsx', 'gif', 'GIF', 'doc', 'docx', 'ppt', 'pptx']
-        f = request.files['file1']
+        file_class = ['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG',
+                      'xlsx', 'gif', 'GIF', 'doc', 'docx', 'ppt', 'pptx']
+        f = request.files.getlist('file1')
+        T_F=''
         Path = './static/uploads'
-        src_name = f.filename.rsplit('.', 1)[0]
-        ext = f.filename.rsplit('.', 1)[1]
-        if ext in file_class:
-            unix_time = int(time.time())
-            new_filename = random_plus().random_plus(EN='T')+str(unix_time)
-            f.save(os.path.join(Path, new_filename+'.'+ext))
-            T = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            data = {'src_name': src_name, 'sys_name': new_filename,'upload_time': T, 'file_type': ext}
-            sqlite3_plus.sqlite3_plus(Path='./sql/upload.db', Tab='upload').add(Data=data)
-            return 'done'
-        else:
-            return 'error'
+        for x in f:
+            print(x)
+            src_name = x.filename.rsplit('.', 1)[0]
+            ext = x.filename.rsplit('.', 1)[1]
+            if ext in file_class:
+                unix_time = int(time.time())
+                new_filename = random_plus().random_plus(EN='T')+str(unix_time)
+                x.save(os.path.join(Path, new_filename+'.'+ext))
+                T = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                data = {'src_name': src_name, 'sys_name': new_filename,
+                        'upload_time': T, 'file_type': ext}
+                sqlite3_plus.sqlite3_plus(
+                    Path='./sql/upload.db', Tab='upload').add(Data=data)
+                T_F='done'
+            else:
+                T_F='error'
+        return T_F
     else:
         return 'error'
 
 
 if __name__ == "__main__":
-    app.run(port=80, threaded=True, debug=True)
+    app.run(port=80, host='0.0.0.0', debug=True)
